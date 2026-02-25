32e2df41a9a0dc1abbbb8c6a018881e7814896ff
dhiaa67 <dhiaa67@github.com>
2026-02-26 00:48:11 +0300

    fix: v0.0269

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -33,25 +33,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="ltr">
+    <html lang="en" dir="ltr" className={ibmPlexArabic.variable}>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Google+Sans:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Google+Sans+Text:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body
-        className={`${ibmPlexArabic.variable} antialiased`}
-      >
+      <body className="antialiased">
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
