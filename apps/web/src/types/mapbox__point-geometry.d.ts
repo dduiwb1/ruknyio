22e1bc0ff54bd052202f6231228c15e1b9fe3de0
dhiaa67 <dhiaa67@github.com>
2026-02-27 19:12:06 +0300

    fix: v0.0294

---
+++ apps/web/src/types/mapbox__point-geometry.d.ts
@@ -0,0 +1,4 @@
+// This file was created to remove the implicit type reference for 'mapbox__point-geometry'.
+// If you need to use this module, add its type definitions here.
+// Otherwise, this file will prevent TypeScript build errors.
+declare module 'mapbox__point-geometry';
